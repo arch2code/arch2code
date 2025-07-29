4a3e0fb43693a993cdb245b1a99769713aa5c729
Gregory Augier <gregory.augier@latticesemi.com>
2025-07-29 15:08:41 -0700

    #14 hwRegister model with interface

--- common/systemc/hwRegister.h
+++ common/systemc/hwRegister.h
@@ -70,5 +70,63 @@ private:
     sc_bv<N*8> m_val_sc;
 };
 
+template <class REG_DATA, class PORT, int N=4>
+class hwRegisterIf : public regBase
+{
+public:
+    hwRegisterIf(PORT *port_) : m_port(port_)
+    {
+       memset(&m_val, 0, sizeof(m_val));
+    }
+    uint32_t cpu_read(uint64_t address) override
+    {
+        Q_ASSERT_CTX(address < N, "", "Address out of range");
+        uint32_t val;
+        m_val_sc = m_val.sc_pack();
+        val = (uint32_t) m_val_sc.range(8*address+31, 8*address).to_uint64();
+        return val;
+    }
+    void cpu_write(uint64_t address, uint32_t val) override
+    {
+        Q_ASSERT_CTX(address < N, "", "Address out of range");
+        m_val_sc = m_val.sc_pack();
+        m_val_sc.range(8*address+31, 8*address) = val;
+        m_val.sc_unpack(m_val_sc);
+        if (address == (N-4))
+        {
+            (*m_port)->write(m_val);
+        }
+        if (m_event != nullptr && address == (N-4))
+        {
+            m_event->notify();
+        }
+    }
+    REG_DATA read(void)
+    {
+        return m_val;
+    }
+    void write(REG_DATA val)
+    {
+        m_val = val;
+        (*m_port)->write(m_val);
+        if (m_event != nullptr)
+        {
+            m_event->notify();
+        }
+    }
+    void copy(REG_DATA& to)
+    { 
+        to = *this; 
+    }  
+    void registerEvent(sc_event *event)
+    {
+        m_event = event;
+    }
+    REG_DATA m_val;
+    sc_event *m_event=nullptr;
+private:
+    sc_bv<N*8> m_val_sc;
+    PORT *m_port;
+};
 
 #endif //(HWREGISTER_H)
