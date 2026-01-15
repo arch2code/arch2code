082833b06deaa0137e7a91ccf087ed5c76a35c40
Gregory Augier <gregory.augier@latticesemi.com>
2026-01-15 13:40:43 -0800

    #90 update dotF file for compilation with 3rd party simulator

--- common/systemVerilog/a2c.f
+++ common/systemVerilog/a2c.f
@@ -1,18 +1,31 @@
 +libext+.sv
-+incdir+.
 +incdir+../../interfaces/apb
+-y ../../interfaces/apb
 +incdir+../../interfaces/status
+-y ../../interfaces/status
 +incdir+../../interfaces/rdy_vld
+-y ../../interfaces/rdy_vld
 +incdir+../../interfaces/req_ack
+-y ../../interfaces/req_ack
 +incdir+../../interfaces/push_ack
+-y ../../interfaces/push_ack
 +incdir+../../interfaces/pop_ack
+-y ../../interfaces/pop_ack
 +incdir+../../interfaces/external_reg
+-y ../../interfaces/external_reg
 +incdir+../../interfaces/memory
+-y ../../interfaces/memory
 +incdir+../../interfaces/notify_ack
+-y ../../interfaces/notify_ack
 +incdir+../../interfaces/axi_read
+-y ../../interfaces/axi_read
 +incdir+../../interfaces/axi_write
+-y ../../interfaces/axi_write
 +incdir+../../interfaces/axi4_stream
+-y ../../interfaces/axi4_stream
 +incdir+../../interfaces/raw
+-y ../../interfaces/raw
++incdir+.
 -y .
 flops.sv
 asserts.svh
