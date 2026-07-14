6791451518a43bf2631549beeb8f720442582c3b
Andy Tomlin <andy.tomlin@latticesemi.com>
2026-07-13 17:40:56 -0700

    #116 add fw bsp

--- include/make/a2c-systemc.mk
+++ include/make/a2c-systemc.mk
@@ -45,7 +45,7 @@ CXX_FLAGS = -m64 -std=$(CPP_STD) -g -Wfatal-errors -Wall -Wextra -Wpedantic -Wsh
 LD_FLAGS = -lboost_system -lboost_program_options -lboost_stacktrace_basic -L$(LD_BOOST) -L$(SYSTEMC_LIBDIR) -ldl -lrt -lsystemc
 CPP_INCLUDES = -I$(BOOST_INCLUDE) -I$(SYSTEMC_INCLUDE) -I/usr/local/include
 
-A2C_SRC_DIRS = $(A2C_ROOT)/common/systemc $(A2C_ROOT)/common/scmain $(wildcard $(A2C_ROOT)/interfaces/*) $(wildcard $(A2C_ROOT)/pro/interfaces/*)
+A2C_SRC_DIRS = $(A2C_ROOT)/common/systemc $(A2C_ROOT)/common/systemc/bsp $(A2C_ROOT)/common/scmain $(wildcard $(A2C_ROOT)/interfaces/*) $(wildcard $(A2C_ROOT)/pro/interfaces/*)
 # Project C++ source/include dirs come from the generated manifest (.gen/build.mk,
 # included by a2c-common.mk) instead of globbing fixed functional roots; the
 # per-dir file wildcards below pick up every .cpp within them.
